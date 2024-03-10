f1518345b8e75ecbe0951a79b56dfe01ab716317
Rohan Bandaru <rohanbandaru@ucsb.edu>
2024-03-10 04:33:14 -0700

    bruhhruh

---
+++ src/drone/DroneConstants.java
@@ -0,0 +1,7 @@
+package drone;
+
+public class DroneConstants {
+    public final static double MASS = 2; // total mass of drone in kg
+    final static double X_MOI = 0;
+    final static double Y_MOI = 0;
+}
--- src/orientation/Orientation.java
+++ src/orientation/Orientation.java
@@ -1,5 +1,6 @@
 package orientation;
 
+import static drone.DroneConstants.*;
 import static java.lang.Math.*;
 
 public class Orientation {
@@ -7,17 +8,27 @@ public class Orientation {
     Quaternion orientation_raw_gyro = Quaternion.IDENTITY;
     Quaternion orientation = Quaternion.IDENTITY;
 
-    public void update(double dt, Vector3 gyro_rates, Vector3 accel) {
+    public Quaternion from_gyro_rates(double dt, Vector3 gyro_rates) {
         double angle = dt * gyro_rates.norm();
         Vector3 n = gyro_rates.normalized();
         double sa = sin(angle / 2);
-        Quaternion d_gyro = new Quaternion(cos(angle / 2), n.x() * sa, n.y() * sa, n.z() * sa);
-        orientation_raw_gyro = orientation_raw_gyro.mul(d_gyro);
+        return new Quaternion(cos(angle / 2), n.x() * sa, n.y() * sa, n.z() * sa);
+    }
+
+    public void update(double dt, Vector3 gyro_rates, Vector3 accel, double thrust) {
+        orientation_raw_gyro = orientation_raw_gyro.mul(from_gyro_rates(dt, gyro_rates));
 
         // accelerations are body-frame
-        Vector3 expected_accel =
 
-        Vector3 gravity = Vector3.of(1, 0, 0).rotatedBy(orientation_raw_gyro);
+        // Estimate acceleration due to motors, drag, etc.
+        Vector3 expected_accel = Vector3.of(0, 0, thrust/MASS);
+        Vector3 compensated_accel = accel.add(expected_accel.scale(-1));
+
+        Vector3 gravity = Vector3.of(1, 0, 0).rotatedBy(orientation);
+
+
+
+
 
 
     }
